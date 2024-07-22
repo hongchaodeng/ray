61e7171694915701554034c6dd84e9b517e25ba9
hongchaodeng <hongchaodeng1@gmail.com>
2024-07-21 21:22:15 -0700

    update

--- release/perf_metrics_scripts/fetch.py
+++ release/perf_metrics_scripts/fetch.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 import json
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 import requests
 import click
 
@@ -13,15 +13,15 @@ PERF_RESULTS_TO_FETCH = {
     r"^many_nodes.aws \(.+\)$": "benchmarks/many_nodes.json",
     r"^many_pgs.aws \(.+\)$": "benchmarks/many_pgs.json",
     r"^many_tasks.aws \(.+\)$": "benchmarks/many_tasks.json",
-    # r"^object_store.aws \(.+\)$": "scalability/object_store.json",
-    # r"^single_node.aws \(.+\)$": "scalability/single_node.json",
-    # r"^stress_test_dead_actors.aws \(.+\)$": (
-    #     "stress_tests/stress_test_dead_actors.json"
-    # ),
-    # r"^stress_test_many_tasks.aws \(.+\)$": "stress_tests/stress_test_many_tasks.json",
-    # r"^stress_test_placement_group.aws \(.+\)$": (
-    #     "stress_tests/stress_test_placement_group.json"
-    # ),
+    r"^object_store.aws \(.+\)$": "scalability/object_store.json",
+    r"^single_node.aws \(.+\)$": "scalability/single_node.json",
+    r"^stress_test_dead_actors.aws \(.+\)$": (
+        "stress_tests/stress_test_dead_actors.json"
+    ),
+    r"^stress_test_many_tasks.aws \(.+\)$": "stress_tests/stress_test_many_tasks.json",
+    r"^stress_test_placement_group.aws \(.+\)$": (
+        "stress_tests/stress_test_placement_group.json"
+    ),
 }
 
 
@@ -32,7 +32,7 @@ except KeyError:
     sys.exit(1)
 
 
-def list_builds(branch: str, commit: str):
+def list_builds(branch: str, commit: Optional[str] = None)-> List[Dict[str, Any]]:
     print("Listing builds")
     url = "https://api.buildkite.com/v2/organizations/ray-project/pipelines/release/builds"
     headers = {
@@ -41,25 +41,23 @@ def list_builds(branch: str, commit: str):
     }
     params = {
         "branch": branch,
-        "commit": commit,
-        # "state": "passed",
-        # "created_from": "2024-07-13T23:34:38Z", # ISO 8601 format
     }
+    if commit:
+        params["commit"] = commit
 
     # Make the GET request to the BuildKite API
     response = requests.get(url, headers=headers, params=params)
 
-    if response.status_code == 200:
-        # Parse the JSON response
-        builds = response.json()
-        return builds
-    else:
+    if response.status_code != 200:
         raise Exception(
             f"Failed to fetch builds: {response.status_code}"
         )
 
+    builds = response.json()
+    return builds
 
-def list_artifacts(branch, commit, build_number, job_id) -> List[Dict[str, Any]]:
+
+def list_artifacts(build_number, job_id) -> List[Dict[str, Any]]:
     print(f"Listing artifacts, {build_number=}, {job_id=}")
     url = (
         "https://api.buildkite.com/v2/organizations/ray-project/pipelines/release"
@@ -70,13 +68,7 @@ def list_artifacts(branch, commit, build_number, job_id) -> List[Dict[str, Any]]
         'Authorization': f'Bearer {buildkite_token}',
         'Content-Type': 'application/json',
     }
-    params = {
-        "branch": branch,
-        "commit": commit,
-        # "state": "passed",
-        # "created_from": "2024-07-13T23:34:38Z", # ISO 8601 format
-    }
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
         raise Exception(
             f"Failed to list artifacts: {response['json']}"
@@ -84,7 +76,7 @@ def list_artifacts(branch, commit, build_number, job_id) -> List[Dict[str, Any]]
     return response.json()
 
 
-def download_artifact(branch, commit, build_number, job_id, artifact_id) -> bytes:
+def download_artifact(build_number, job_id, artifact_id) -> bytes:
     """
     Download artifact from a given artifact ID. The result is in the format:
     - {"type": "<type_of_content>", "content": <content>}
@@ -101,13 +93,7 @@ def download_artifact(branch, commit, build_number, job_id, artifact_id) -> byte
         'Authorization': f'Bearer {buildkite_token}',
         'Content-Type': 'application/json',
     }
-    params = {
-        "branch": branch,
-        "commit": commit,
-        # "state": "passed",
-        # "created_from": "2024-07-13T23:34:38Z", # ISO 8601 format
-    }
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
         raise Exception(
             f"Failed to download artifact: {response.json()}"
@@ -115,36 +101,28 @@ def download_artifact(branch, commit, build_number, job_id, artifact_id) -> byte
     return response.content
 
 
-def find_and_retrieve_artifact_content(branch, commit, build_number, job_id, artifact_filename) -> bytes:
+def find_and_retrieve_artifact_content(build_number, job_id, artifact_filename) -> bytes:
     """Look for artifact file and retrieve its content."""
-    artifacts = list_artifacts(branch, commit, build_number, job_id)
+    artifacts = list_artifacts(build_number, job_id)
     for artifact in artifacts:
         if "filename" not in artifact:
             continue
         if artifact["filename"] == artifact_filename:
-            result = download_artifact(
-                branch,
-                commit,
-                build_number,
-                job_id,
-                artifact["id"],
-            )
+            result = download_artifact(build_number, job_id, artifact["id"])
             return result
     raise Exception(f"Artifact {artifact_filename} not found")
 
-def download_results(branch, commit, builds):
+def fetch_results(branch, commit, builds):
     print("Downloading results")
     perf_results_to_fetch = PERF_RESULTS_TO_FETCH.copy()
     fetched_results = {}
     for build in builds:
-        print(f"build: {build['number']}, state: {build['state']}")
         for job in build["jobs"]:
             if "name" in job:
                 for job_regex, file_name in list(perf_results_to_fetch.items()):
                     if re.match(job_regex, job["name"]):
+                        print(f"matched job: {job['name']}")
                         artifact_content = find_and_retrieve_artifact_content(
-                            branch,
-                            commit,
                             build["number"],
                             job["id"],
                             "result.json",
@@ -155,20 +133,20 @@ def download_results(branch, commit, builds):
                         perf_results_to_fetch.pop(job_regex)
                         print(f"Fetched {file_name=}")
 
-
-                        
-    with open("tmp~/perf/result.json", "w") as f:
-        json.dump(fetched_results, f, indent=2)
+    return fetched_results
 
 
 @click.command()
 @click.option("--branch", type=str, default="master")
 @click.option("--commit", required=True, type=str)
-def main(branch: str, commit: str):
+@click.option("--outputdir", required=True, type=str)
+def main(branch: str, commit: str, outputdir: str):
     print(f"Branch: {branch}, Commit: {commit}")
     builds = list_builds(branch, commit)
-    download_results(branch, commit, builds)
+    results = fetch_results(branch, commit, builds)
 
+    with open(os.path.join(outputdir, "result.json"), "w") as f:
+        json.dump(results, f, indent=2)
 
 if __name__ == "__main__":
     main()
